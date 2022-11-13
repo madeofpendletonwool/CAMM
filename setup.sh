8883efbd414ffef3354c3a252d62b87c3e5ddfeb
brickinthewall10 <blacanwite@gmail.com>
2022-11-12 20:13:00 -0600

    Changing setup again

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
  #!/bin/bash
 #write out current crontab
-crontab -l > CAMMcron
+# crontab -l > CAMMcron
 #echo new cron into cron file
 # echo "$1 /usr/bin/python3 /data/CAMM.py $2" >> CAMMcron
 (crontab -l 2>/dev/null; echo "$1 /usr/bin/python3 /data/CAMM.py $2") | crontab -
 #install new cron file
-crontab CAMMcron
+# crontab CAMMcron
