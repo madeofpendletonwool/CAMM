c912d75e38af84a887c6e98d1dbcd869ee2ee447
brickinthewall10 <blacanwite@gmail.com>
2022-11-12 20:08:46 -0600

    Updating setup file to support scripts

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 #write out current crontab
 crontab -l > CAMMcron
 #echo new cron into cron file
-echo "$1 /usr/bin/python3 /data/CAMM.py $2" >> CAMMcron
+# echo "$1 /usr/bin/python3 /data/CAMM.py $2" >> CAMMcron
+(crontab -l 2>/dev/null; echo "$1 /usr/bin/python3 /data/CAMM.py $2") | crontab -
 #install new cron file
 crontab CAMMcron
